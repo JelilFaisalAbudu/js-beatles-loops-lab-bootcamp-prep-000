02aa6f24bac4428c078a9b205242f9894a94d5cf
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-10 08:26:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function theBeatlesPlay(musicians, instruments) {
   let arr = [];
   
   for (let i = 0; i < musicians.length; i++) {
-    
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+  
+  return arr;
 }
